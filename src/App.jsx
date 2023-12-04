9ebbb0e0b8156957d20a209ad8b1509cef381ee5
hashmi7917 <hmuqtadeer@gmail.com>
2023-12-04 17:00:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='max-w-xs'>
+      <h1>Tailwind Starter React+Vite+React</h1>
+    </div>
+  )
+}
+
+export default App
